2537651f4988975c080ad88d7f7ed223d30ae735
Romain Gaboriau <romain.gaboriau@edu.devinci.fr>
2023-02-03 12:31:46 +0100

    romain

---
+++ src/components/App/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+
+function App() {
+  return (
+    <></>
+  );
+}
+
+export default App;
